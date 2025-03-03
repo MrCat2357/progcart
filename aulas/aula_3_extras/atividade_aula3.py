ee4ab4809d8e471ecc158175af7ddbae76b93462
MrCat2357 <catuloaxel.mat@gmail.com>
2025-03-03 11:18:04 -0300

    mudando a pasta destino para dentro do qgis

--- aulas/aula_3_extras/atividade_aula3.py
+++ aulas/aula_3_extras/atividade_aula3.py
@@ -1,5 +1,6 @@
 import wget
 import os
+import zipfile
 
 class Downloader:
     def __init__(self, server_url_format, destination_folder):
@@ -18,6 +19,22 @@ class Downloader:
         wget.download(url, file_path)
         print(f"\nArquivo {file_name} baixado com sucesso para {file_path}.")
 
+        # Descompactar o arquivo, se for um ZIP
+        if file_name.endswith(".zip"):
+            self.unzip_file(file_path)
+
+    def unzip_file(self, zip_path):
+        # Verificar se o arquivo ZIP existe
+        if zipfile.is_zipfile(zip_path):
+            print(f"Descompactando {zip_path}...")
+            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+                zip_ref.extractall(self.destination_folder)
+            print(f"Arquivo {zip_path} descompactado com sucesso.")
+            # Após descompactar, você pode opcionalmente excluir o arquivo ZIP
+            os.remove(zip_path)
+        else:
+            print(f"O arquivo {zip_path} não é um arquivo ZIP válido.")
+
 class IBGE(Downloader):
     def __init__(self, destination_folder):
         server_url_format = "https://geoftp.ibge.gov.br/cartas_e_mapas/folhas_topograficas/vetoriais/escala_1000mil/shapefile/{name}"
@@ -29,7 +46,7 @@ class IBGE(Downloader):
 # Exemplo de uso
 if __name__ == "__main__":
     # Defina a pasta de destino
-    destination_folder = "C:/Users/catul/Downloads/"
+    destination_folder = "C:/Program Files/QGIS 3.40.3/apps/qgis/resources/data/camadas_ibge/"
 
     # Criação do objeto IBGE
     ibge_downloader = IBGE(destination_folder)
@@ -37,4 +54,3 @@ if __name__ == "__main__":
     # Baixando um arquivo específico
     file_name = "g04_na19.zip"  # Substitua pelo nome do arquivo real
     ibge_downloader.download_data(file_name)
- 
